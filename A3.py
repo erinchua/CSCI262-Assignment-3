6f5fb65ee7d16b5ec416f08b4942310a298f14ed
Vincent Yap <the-mazer@hotmail.com>
2020-11-15 22:44:02 +0800

    reading of files

--- A3.py
+++ A3.py
@@ -1,5 +1,12 @@
-i = 0
+import os
 
-while i < 3:
-    print("hello world " + str(i))
-    i+=1
+currentDir = os.path.dirname(os.path.abspath(__file__))
+eventFileDir = os.path.join(currentDir, 'Events.txt')
+statsFileDir = os.path.join(currentDir, 'Stats.txt')
+
+eventFile = open(eventFileDir, "r")
+statsFile = open(statsFileDir, "r")
+
+
+print(eventFile.read())
+print(statsFile.read())
