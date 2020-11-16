37c8a3804cd398dc24ca0d3c253d6cc6b76019e4
Erin Chua <erinenin@gmail.com>
2020-11-16 13:51:57 +0800

    split events.txt and stats.txt into nested list

--- A3.py
+++ A3.py
@@ -5,19 +5,26 @@ def initialInput():
     currentDir = os.path.dirname(os.path.abspath(__file__))
     eventFileDir = os.path.join(currentDir, commandArg[1])
     statsFileDir = os.path.join(currentDir, commandArg[2])
-    noOfDays = commandArg[3]
 
     eventFile = open(eventFileDir, "r")
     statsFile = open(statsFileDir, "r")
 
-    print(eventFile.read() + "\n")
-    print(statsFile.read() + "\n")
+    # print(eventFile.read() + "\n")
+    # print(statsFile.read() + "\n")
 
-    # eventFileList = eventFile.read().split("\n")
+    eventFileList = [item.split("\n") for item in eventFile.read().split("\n")]
+    print(eventFileList)
+    
+    for i in range(len(eventFileList)):
+        print(eventFileList[i])
 
-    # for i in range(len(eventFileList)):
-    #     splitByColon = eventFileList[i].split(":")
-    #     print(splitByColon)
+    print("\n")
+    statsFileList = [item.split("\n") for item in statsFile.read().split("\n")]
+    print(statsFileList)
+    
+    for i in range(len(statsFileList)):
+        print(statsFileList[i])
+            
 
 if __name__ == "__main__":
     initialInput()
