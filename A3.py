5cff7349550eb94f1563b058f2bb4e75f0845b16
Erin Chua <erinenin@gmail.com>
2020-11-16 12:19:01 +0800

    Auto stash before merge of "ErinBranch" and "origin/vincent"

--- A3.py
+++ A3.py
@@ -1,13 +1,25 @@
 import os, sys
 
-commandArg = sys.argv
-currentDir = os.path.dirname(os.path.abspath(__file__))
-eventFileDir = os.path.join(currentDir, commandArg[1])
-statsFileDir = os.path.join(currentDir, commandArg[2])
-noOfDays = commandArg[3]
+def initialInput():
+    commandArg = sys.argv
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+    eventFileDir = os.path.join(currentDir, commandArg[1])
+    statsFileDir = os.path.join(currentDir, commandArg[2])
+    noOfDays = commandArg[3]
 
-eventFile = open(eventFileDir, "r")
-statsFile = open(statsFileDir, "r")
+    eventFile = open(eventFileDir, "r")
+    statsFile = open(statsFileDir, "r")
 
-print(noOfDays + "\n" + eventFile.read() + "\n")
-print(noOfDays + "\n" + statsFile.read() + "\n")
+    eventFileList = eventFile.read().split("\n")
+
+    print(eventFileList)        
+
+    for i in range(len(eventFileList)):
+        splitByColon = eventFileList[i].split(":")
+        print(splitByColon)
+    # print(noOfDays + "\n" + eventFile.read() + "\n")
+    # print(noOfDays + "\n" + statsFile.read() + "\n")
+
+
+if __name__ == "__main__":
+    initialInput()
